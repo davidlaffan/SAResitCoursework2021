a86d55d010d6bb7faf61e64be63b1d0ae58bbbf5
davidlaffan <46089582+davidlaffan@users.noreply.github.com>
2021-05-30 13:59:40 +0100

    Part 1 finished
    Pet names will come with capitalised first letter and the rest lowercase

--- src/Pet.java
+++ src/Pet.java
@@ -3,7 +3,7 @@ public class Pet{
 	public String type;
 	//arraylist of food
 	public Pet(String t) {
-		this.type = t;
+		this.type = t; //Will have the first letter uppercase and rest lowercase
 	}
 	public int compareTo(Pet arg0) {
 		return type.compareTo(arg0.type);
--- src/main.java
+++ src/main.java
@@ -12,16 +12,46 @@ static BinarySearchTree pets = new BinarySearchTree();
 	    System.out.println("Enter pet type"); 
 	    petName = myObj.nextLine();   
 	    newPet(petName);
-	    
+	    showAll();
 		//TODO new product for pet
 		//TODO search for pet AND see if any products exist
 		//TODO list all pets
 		//
 	}
 	public static void newPet(String n){
+		n = upperCaseFirst(n);
 		Pet i = new Pet (n);
 		pets.addNode(i); //https://gist.github.com/provencher/11357144 ??
 	}
 	
-
+	public static void findPet(String i) {
+		i = upperCaseFirst(i);
+		Node r = pets.findNode(i);
+		if (r.key == null) {
+			System.out.println("Not found");
+		}
+		else {
+			System.out.println(r.key.toString());
+		}
+	}
+	
+	public static void removePet() {
+		
+	}
+	
+	public static void showAll() {
+		pets.inOrderTraverseTree(pets.root);
+	}
+	
+	public static String upperCaseFirst(String value) {
+		value = value.toLowerCase();
+        // Convert String to char array.
+        char[] array = value.toCharArray();
+        // Modify first element in array.
+        array[0] = Character.toUpperCase(array[0]);
+        // Return string.
+        return new String(array);
+    }//modified from https://www.dotnetperls.com/uppercase-first-letter-java
+	
+	
 }
